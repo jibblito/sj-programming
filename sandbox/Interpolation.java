2f5f3c9424c06e3be5a940b943eac88a9083b2ed
jibblito <roryhibbler@aol.com>
2019-04-11 19:55:39 +0000

    Finished Interpolation.java for now....

--- sandbox/Interpolation.java
+++ sandbox/Interpolation.java
@@ -1,62 +1,88 @@
 import java.util.Scanner;
 import java.awt.Point;
 
-public class Interpolaton {
+public class Interpolation {
 	
 	public static void main(String[] args) {
 	
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Points:");
-		int ax = scan.nextInt;
-		int ay = scan.nextInt;
-		int bx = scan.nextInt;
-		int by = scan.nextInt;
-		int cx = scan.nextInt;
-		int cy = scan.nextInt;
+		int ax = scan.nextInt();
+		int ay = scan.nextInt();
+		float av = scan.nextFloat();
+		int bx = scan.nextInt();
+		int by = scan.nextInt();
+		float bv = scan.nextFloat();
+		int cx = scan.nextInt();
+		int cy = scan.nextInt();
+		float cv = scan.nextFloat();
 		System.out.println();
 
-		Triangle tr = new Triangle(new Point(ax,ay), new Point(bx,by), new Point(cx,cy));
-		tr.toString();
+		Triangle tr = new Triangle(new Pair(new Point(ax,ay),av),new Pair(new Point(bx,by),bv),new Pair(new Point(cx,cy),cv));
+		tr.catString();
+		tr.sortPoints(0);
+		tr.catString();
 	}	
 	
 }
 
 class Triangle {
 	
-	private Point a;
-	private Point b;
-	private Point c;
-	private Point[] points = new Point[3];
+	private Pair a;
+	private Pair b;
+	private Pair c;
+	private Pair[] pairs = new Pair[3];
 
-	public Triangle(Point a, Point b, Point c) {
+	public Triangle(Pair a, Pair b, Pair c) {
 		this.a = a; this.b = b; this.c = c;
-		points[3] = {this.a,this.b,this.c};
+		pairs = new Pair[]{this.a,this.b,this.c};
 	}
 
 	public void sortPoints(int start) {
 		if (start == 2) return;
-		Point max = new Point(0,0);
+		Pair max = new Pair(new Point(0,0),0.0f);
 		int maxInd = 0;
-
-		for(int i = start; i < points.length; i++) {
-			if(points[i].y > max.y) {
-				max = points[i];
+		for(int i = start; i < pairs.length; i++) {
+			if(pairs[i].getPoint().y > max.getPoint().y) {
+				max = pairs[i];
 				maxInd = i;
 			}
 		}
-		
-		point temp = points[start];
-		points[start] = max;
-		points[maxInd] = temp;
-		sortPoints(start + 1);
 
+		Pair temp = pairs[start];
+		pairs[start] = max;
+		pairs[maxInd] = temp;
+		sortPoints(start + 1);
 	}
 
-	public void toString() {
-		System.out.println("Points:\nA: " + points[0].y);
-		System.out.println("B: " + points[1].y);
-		System.out.println("C: " + points[2].y);
+	public void catString() {
+		System.out.println("A: " + pairs[0].getPoint().y + " " +pairs[0].getVal());
+		System.out.println("B: " + pairs[1].getPoint().y + " " + pairs[1].getVal());
+		System.out.println("C: " + pairs[2].getPoint().y + " " + pairs[2].getVal());
 	}
 
 
 }
+
+class Pair {
+	private Point p;
+	private float val;	
+
+	public Pair(Point p, Float val) {
+		this.p = p;
+		this.val = val;
+	}
+
+	public Point getPoint() {
+		return p;
+	}
+	public void setPoint(Point p) {
+		this.p = p;
+	}
+
+	public Float getVal() {
+		return val;
+	}
+}
+
+
