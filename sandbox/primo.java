a4080307297a0b7336e8447d66b7316e5fcbc778
jibblito <roryhibbler@aol.com>
2019-04-11 06:55:25 +0000

    primo.java added (as well as .class file)

--- sandbox/primo.java
+++ sandbox/primo.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public primo {
+public class primo {
 		
-	String version = "1.0";	
+	static String version = "1.0";	
 
 	public static void main(String[] args) {
 		int numChar;
+		int numPass;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("PASS GEN VERSION " + version + " BY RORY HIBBLER");
 		System.out.print("Number of characters: ");
 		numChar = scan.nextInt();
-		System.out.println("\n==================\nGenerating " + numChar + " Char pass...");
+		System.out.print("Number of passwords: ");
+		numPass = scan.nextInt();
+		System.out.println();
+		System.out.println("======================");
+		if(numPass == 1) System.out.println("Generating 1 password with length  " + numChar + "...");
+		else System.out.println("Generating " + numPass + " passwords with length " + numChar + "...");
 		
+		System.out.println();
 		
 		String pass = "";
-		for(int i = 0; i < numChar; i++) {
-			pass += (char)((int)(Math.random() * 93)+33);
+		for(int i = 0; i < numPass; i++) {
+			for(int j = 0; j < numChar; j++) {
+				pass += (char)((int)(Math.random() * 93)+33);
+			}
+			System.out.println(pass);
+			pass = "";
 		}
-		System.out.println(pass);
+		System.out.println();
 		System.out.println("Process Finished");
 		
 	}
