985ff43ff70eb16f9f1ad80022400a3bc93c7b06
jibblito <roryhibbler@aol.com>
2019-05-13 14:01:55 +0000

    Added WindowPrimo and updated files

--- sandbox/jibblito/WindowPrimo.java
+++ sandbox/jibblito/WindowPrimo.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
 import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
 
 public class WindowPrimo extends JFrame {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Window Prime");
-		frame.getContentPane().setLayout(new FlowLayout());
 		frame.setSize(600,600);
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label1 = new JLabel("D'oh",SwingConstants.CENTER);
+		label1.setPreferredSize(new Dimension(300,300));
+		frame.getContentPane().add(label1,BorderLayout.CENTER);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }
